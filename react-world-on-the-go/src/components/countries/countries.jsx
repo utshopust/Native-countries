e31b00270131d645e66320caa86b204c47d0582a
utshopust <mcutsho96@gmail.com>
2025-10-26 20:14:31 +0600

    Lift up state in arrays by comparing reference

--- react-world-on-the-go/src/components/countries/countries.jsx
+++ react-world-on-the-go/src/components/countries/countries.jsx
@@ -7,21 +7,27 @@ const countries = ({ countriesPromise }) => {
   const [visitedCountries, setVisitedCountries] = useState([]);
 
   const handleVisitedCountries = (country) => {
-    console.log("country visited clicked",country);
+    console.log("country visited clicked", country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
   };
 
   return (
     <>
       <h2 className="center">Total countries: {countries.length}</h2>
-      <h3>Traveled So Far: </h3>
+      <h3>Traveled So Far: {visitedCountries.length}</h3>
+      <ol>
+         {
+          visitedCountries.map(country=><li>{country.name.common}</li>)
+         }
+      </ol>
       <div className="countries">
         {countries.map((country) => (
-          <Country 
-          key={country.ccn3} 
-          handleVisitedCountries={handleVisitedCountries}
-          country={country}>
-
-          </Country>
+          <Country
+            key={country.ccn3}
+            handleVisitedCountries={handleVisitedCountries}
+            country={country}
+          ></Country>
         ))}
       </div>
     </>
