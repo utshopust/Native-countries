1358aafa0d89a2e840c8a26e30450c39c3898b57
utshopust <mcutsho96@gmail.com>
2025-10-26 11:44:58 +0600

    Conditional CSS and Toggle state based on click

--- react-world-on-the-go/src/components/country/country.jsx
+++ react-world-on-the-go/src/components/country/country.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./country.css";
 
 const country = ({ country }) => {
+    const [visited, setVisited] = useState(false)
+
+
   const handleVisited = () => {
-    console.log("visited clicked");
+    // if(visited === true){
+    //     setVisited(false);
+    // }
+    // else{
+    //     setVisited(true);
+    // }
+    setVisited(!visited);
   };
   return (
-    <div className="country">
+    <div className={`country ${visited && 'country-visited'}`}>
       <h3>Name: {country.name.common}</h3>
       <img src={country.flags.png} alt="" />
       <p>Independent: {country.independent ? "Free" : "Not Free"}</p>
       <p>Population: {country.population}</p>
-      <button onClick={handleVisited}>Not Visited</button>
+      <button onClick={handleVisited}>{
+        visited ? 'Visited' : 'Not Visited'
+        }</button>
     </div>
   );
 };
